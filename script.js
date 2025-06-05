6d4a782fdd65418ac5da423f7ab59abbe776ebef
Nickel <85444719+TamplierNik@users.noreply.github.com>
2025-06-05 14:07:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,107 @@
-let clock2 = document.getElementById("clock2");
-let clock3 = document.getElementById("clock3");
-let clock4 = document.getElementById("clock4");
-let clock5 = document.getElementById("clock5");
-let clock6 = document.getElementById("clock6");
-let clock7 = document.getElementById("clock7");
-
 // if (masive[3] == true)
 //     star1.src = './images/Icon_star_empty.png'
 // else
 //     masive[3] = false
 
-let catalog = document.querySelectorAll(".div_catalog");
-
-console.log(catalog)
-
-const catalog_items = {
-        image: [clock2, clock2, clock4, clock5, clock6, clock7], 
+const catalog_items = [
+    {
+        image: "./images/clock2.png", 
         title: 'Амфибия 2043', 
         price: '9 800₽', 
         is_favourite: true
-    }
-
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+    {
+        image: "./images/clock3.png", 
+        title: 'Амфибия 1524', 
+        price: '10 800₽', 
+        is_favourite: false
+    },
+]
 console.log(catalog_items)
 
-let imgs = document.getElementById("clock2")
-
-console.log(imgs)
+const product = document.querySelectorAll("div_catalog")
 
 // function changeALL(){
-    for(let i=0;i<=catalog.length;i++){
-        catalog[i].innerHTML = catalog_items[0];
-        catalog[i].innerHTML = catalog_items.title;
-        catalog[i].innerHTML = catalog_items.price;
-        catalog[i].insertAdjacentHTML("afterbegin",
-        `<img class="clock2" src="${catalog_items.image}">
-        <p class="vostok1"></p>
-        <p class="katalog_price"></p>`)
+    for(let i=0;i<=catalog_items.length;i++){
+    
+        function renderCatalog(products) {
+        const catalog = document.getElementById('catalog');
+        catalog.innerHTML = '<div class="katalog"></div>';
+
+        products.forEach(product => {
+            const card = document.createElement('div');
+            card.className = 'div_grid1';
+
+            const favoriteIcon = product.is_favourite ? './images/Icon_star.png' : './images/Icon_star_empty.png';
+
+            card.innerHTML = 
+            '<div class="star" data-id="${product.id}" style="cursor: pointer; font-size: 20px;">${favoriteIcon}</div>'
+            '<img src="${catalog_items.image}" alt="${catalog_items.title}" class="clock2">'
+            '<h3 class="vostok1">${catalog_items.title}</h3>'
+            '<p class="katalog_number">${catalog_items.price} ₽</p>'
+            '<button class="korzina_div"><h1 class="korzina">Купить</h1></button>'
+            ;
+
+            catalog.appendChild(card);
+        });
+
+        // Назначаем обработчики после рендера
+        document.querySelectorAll('star').forEach(icon => {
+            icon.addEventListener('click', function () {
+            const id = parseInt(this.dataset.id);
+            const product = products.find(p => p.id === id);
+            product.is_favourite = !product.is_favourite;
+            renderCatalog(products); // перерисовываем
+            });
+        });
+        }
+
+        renderCatalog(product);
     }
 // }
 
-console.log(catalog_items.image);
 
 
 // function changeIMG1(){
@@ -59,15 +121,5 @@ console.log(catalog_items.image);
 //     star2.src = './images/Icon_star_empty.png'
     
 // }
-
-
-
-
-
-
-
-
-
-
 
 // console.log(masive)
