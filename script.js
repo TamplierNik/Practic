930c32e05e1e23eace1b5c440e7eb6c8cb7b1d64
Nickel <85444719+TamplierNik@users.noreply.github.com>
2025-06-25 12:01:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// if (masive[3] == true)
-//     star1.src = './images/Icon_star_empty.png'
-// else
-//     masive[3] = false
-
 const catalog_items = [
     {   
         id: 1,
@@ -14,7 +9,7 @@ const catalog_items = [
     {
         id: 2,
         image: "./images/katalog_clock.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2413', 
         price: '9 800', 
         is_favourite: false,
         id_product: 'clock3'
@@ -22,14 +17,14 @@ const catalog_items = [
     {
         id: 3,
         image: "./images/clock3.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2463', 
         price: '9 800', 
         is_favourite: false
     },
     {
         id: 4,
         image: "./images/clock4.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2483', 
         price: '9 800', 
         is_favourite: false,
         id_product: 'clock4'
@@ -37,7 +32,7 @@ const catalog_items = [
     {
         id: 5,
         image: "./images/clock5.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2443', 
         price: '9 800', 
         is_favourite: false,
         id_product: 'clock5'
@@ -45,7 +40,7 @@ const catalog_items = [
     {
         id: 6,
         image: "./images/clock6.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2053', 
         price: '9 800', 
         is_favourite: false,
         id_product: 'clock6'
@@ -53,30 +48,70 @@ const catalog_items = [
     {
         id: 7,
         image: "./images/clock3.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2683', 
         price: '9 800', 
         is_favourite: false,
     },
     {
         id: 8,
         image: "./images/clock2.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2323', 
         price: '9 800', 
         is_favourite: false,
     },
     {
         id: 9,
         image: "./images/clock9.png", 
-        title: 'восток  амфибия 2403', 
+        title: 'восток  амфибия 2953', 
         price: '9 800', 
         is_favourite: false,
-        id_product: 'clock9'
+        id_product: 'clock9',
+    }
+]
+
+console.log(catalog_items);
+
+const adaptive = [
+    {
+        id: 7,
+        image: "./images/clock3.png", 
+        title: 'восток  амфибия 2683', 
+        price: '9 800', 
+        is_favourite: false,
+    },
+    {
+        id: 8,
+        image: "./images/clock2.png", 
+        title: 'восток  амфибия 2323', 
+        price: '9 800', 
+        is_favourite: false,
+    },
+    {
+        id: 9,
+        image: "./images/clock9.png", 
+        title: 'восток  амфибия 2953', 
+        price: '9 800', 
+        is_favourite: false,
+        id_product: 'clock9',
     }
 ]
 
 function renderCatalog(products) {
+
     const catalog = document.getElementById('catalog');
     catalog.innerHTML = '';
+
+    window.addEventListener('resize', function(event){
+        if(window.innerWidth <= 673){
+            products.splice(-3);
+            renderCatalog(catalog_items);
+        }
+        else{
+            products.push(adaptive[0]);
+            renderCatalog(catalog_items);
+        }
+        
+    });
 
     products.forEach(product => {
         const card = document.createElement('div');
@@ -85,6 +120,7 @@ function renderCatalog(products) {
         const favoriteIcon = product.is_favourite ? './images/Icon_star.png' : './images/Icon_star_empty.png';
 
         card.innerHTML = `
+
         <img class="star" data-id="${product.id}" style="cursor: pointer; font-size: 20px;" 
         src="${favoriteIcon}">
 
@@ -103,9 +139,12 @@ function renderCatalog(products) {
             <button class="korzina_div">
                 <h1 class="korzina">В корзину</h1>
             </button>
+        </div>
         </div>`;
 
         catalog.appendChild(card);
+
+        
     });
 
     // Назначаем обработчики после рендера
@@ -119,29 +158,7 @@ function renderCatalog(products) {
             }
         });
     });
-    }
-
-    renderCatalog(catalog_items);
-
+}
+renderCatalog(catalog_items);
 
 
-
-// function changeIMG1(){
-
-
-//     if (masive[3] == false) {
-//         let star1 = document.getElementById("star1")
-//         star1.src = './images/Icon_star_empty.png'
-//         masive[3] = true
-//     }
-//     else
-//         star1.src = './images/Icon_star.png'
-// }
-
-// function changeIMG2() {
-//     let star2 = document.getElementById("star2")
-//     star2.src = './images/Icon_star_empty.png'
-    
-// }
-
-// console.log(masive)
